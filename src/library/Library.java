0973d5bea2a59a4947c73d11a255fb1fea8ac79d
Kory <Kory@DESKTOP-FTPI8SP.attlocal.net>
2020-03-11 16:08:35 -0500

    Completed table display

--- src/library/Library.java
+++ src/library/Library.java
@@ -18,23 +18,32 @@ package library;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 /**
  *
- * @author Kory
+ * @author Kallory
  */
 public class Library extends Application {
     
     Stage window;
     TableView<Book> table;
+    TextField nameInput;
+    TextField authorInput;
+    TextField genreInput;
+    TextField ISBNInput;
+    TextField yearInput;
+    TextField publisherInput;
 
     /**
      * @param args the command line arguments
@@ -48,6 +57,7 @@ public class Library extends Application {
         window = primaryStage;
         window.setTitle("Library");
         
+//-----------------Column creation--------------------------------------------        
         //Name column
         TableColumn<Book, String> nameColumn = new TableColumn<>("Name");
         nameColumn.setMinWidth(200);
@@ -60,21 +70,99 @@ public class Library extends Application {
         
         //Genre column
         TableColumn<Book, String> genreColumn = new TableColumn<>("Genre");
-        genreColumn.setMinWidth(200);
+        genreColumn.setMinWidth(150);
         genreColumn.setCellValueFactory(new PropertyValueFactory<>("genre"));
+        
+        //ISBN column
+        TableColumn<Book, String> ISBNColumn = new TableColumn<>("ISBN");
+        ISBNColumn.setMinWidth(120);
+        ISBNColumn.setCellValueFactory(new PropertyValueFactory<>("ISBN"));
+        
+        //year column
+        TableColumn<Book, Integer> yearColumn = new TableColumn<>("Year Published");
+        yearColumn.setMinWidth(100);
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("year"));
+        
+        //publisher column
+        TableColumn<Book, String> publisherColumn = new TableColumn<>("Publisher");
+        publisherColumn.setMinWidth(200);
+        publisherColumn.setCellValueFactory(new PropertyValueFactory<>("publisher"));
+        
+//---------------------------Input Fields--------------------------------------
+        //Name input
+        nameInput = new TextField();
+        nameInput.setPromptText("Book name");
+        nameInput.setMinWidth(180);
+        
+        //Author input
+        authorInput = new TextField();
+        authorInput.setPromptText("Author name");
+        authorInput.setMinWidth(160);
+        
+        //Genre input
+        genreInput = new TextField();
+        genreInput.setPromptText("Genre name");
+        genreInput.setMinWidth(130);
+        
+        //ISBN input
+        ISBNInput = new TextField();
+        ISBNInput.setPromptText("Book ISBN 0 if none");
+        ISBNInput.setMinWidth(80);
+        
+        //Year input
+        yearInput = new TextField();
+        yearInput.setPromptText("Year published");
+        yearInput.setMinWidth(80);
+        
+        //Publisher input
+        publisherInput = new TextField();
+        publisherInput.setPromptText("Published by");
+        publisherInput.setMinWidth(100);
+        
+        
+        //Buttons
+        Button addButton = new Button("Add");
+        addButton.setOnAction(e -> addButtonClicked());
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(e -> deleteButtonClicked());
+        
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(8, 8, 8, 8));
+        hBox.setSpacing(8);
+        hBox.getChildren().addAll(nameInput, authorInput, genreInput, ISBNInput, yearInput, publisherInput, addButton, deleteButton);
+        
+        HBox hBox2 = new HBox();
+        hBox2.setPadding(new Insets(10, 10, 10, 10));
+        hBox2.setSpacing(10);
+        hBox2.getChildren().addAll(addButton, deleteButton);
         
         table = new TableView();
         table.setItems(getBook());
-        table.getColumns().addAll(nameColumn, authorColumn, genreColumn);
+        table.getColumns().addAll(nameColumn, authorColumn, genreColumn, ISBNColumn, yearColumn, publisherColumn);
         
         VBox vbox = new VBox();
-        vbox.getChildren().addAll(table);
+        vbox.getChildren().addAll(table, hBox, hBox2);
         
         Scene scene = new Scene(vbox);
         window.setScene(scene);
         window.show();
     }
     
+    //Add button clicked
+    public void addButtonClicked() {
+        Book book = new Book();
+        book.setName(nameInput.getText());
+        book.setAuthor(authorInput.getText());
+        book.setGenre(genreInput.getText());
+        book.setISBN(ISBNInput.getText());
+        book.setYear(Integer.parseInt(yearInput.getText()));
+        book.setPublisher(publisherInput.getText());
+    }
+    
+    //delete button
+    public void deleteButtonClicked() {
+        
+    }
     public ObservableList<Book> getBook() {
         ObservableList<Book> books = FXCollections.observableArrayList();
         books.add(new Book("A Game of Thrones", "G. R. R. Martin", "Fantasy", "9780553573404", 1996, "Bantam Books"));
